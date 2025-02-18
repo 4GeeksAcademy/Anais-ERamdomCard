24554f605f850469a41faa65c96f98bc53436ffd
Anais Escalona <115326780+eanais@users.noreply.github.com>
2025-02-18 23:07:48 +0000

    Random Card

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  generarCarta()
+  document.getElementById("boton").addEventListener("click", generarCarta)
 };
+
+function generarCarta(){
+  let numeros=[2,3,4,5,6,7,8,9,10,"A","J","Q","K"]
+  let numeroRandom =numeros[Math.floor(Math.random() * numeros.length)]
+  console.log(numeroRandom)
+  let palos=[ "♦", "♥", "♠","♣"]
+  let paloRandom =palos[Math.floor(Math.random() * palos.length)]
+  console.log(paloRandom)
+  document.getElementById("palo").innerHTML=paloRandom
+  document.getElementById("numero").innerHTML=numeroRandom
+  document.getElementById("simbolo").innerHTML=paloRandom
+  if (paloRandom=="♥"||paloRandom=="♦"){
+    document.getElementById("palo").style.color="red"
+    document.getElementById("simbolo").style.color="red"
+  } else{
+    document.getElementById("palo").style.color="black"
+    document.getElementById("simbolo").style.color="black"
+  }
+}
